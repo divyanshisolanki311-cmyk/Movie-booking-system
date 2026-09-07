1db846130c7f2fd8d34ab57a859f2e9d62770ace
Divyanshi Solanki <divyanshisolanki311@gmail.com>
2026-09-07 22:42:52 +0530

    Create Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class Customer {
+private:
+    string name, phone;
+
+public:
+    Customer(string name, string phone) : name(name), phone(phone) {}
+    string getName() const { return name; }
+    string getPhone() const { return phone; }
+};
